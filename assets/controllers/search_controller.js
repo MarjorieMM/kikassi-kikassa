bf5028fa41536092e545f8e1acd26d58697c7fcb
Marjorie Meziere <mmwebforce20@gmail.com>
2021-05-28 16:43:58 +0200

    fin refactorisation recherche objets

--- assets/controllers/search_controller.js
+++ assets/controllers/search_controller.js
@@ -1,10 +1,17 @@
 import { Controller } from "stimulus";
+import { useDebounce } from "stimulus-use";
 
 export default class extends Controller {
 	static values = {
 		url: String,
+		objetId: Number,
 	};
-	static targets = ["result"];
+	static targets = ["result", "selected", "objet"];
+	static debounces = ["search"];
+
+	connect() {
+		useDebounce(this);
+	}
 
 	onSearchInput(event) {
 		this.search(event.currentTarget.value);
@@ -17,5 +24,20 @@ export default class extends Controller {
 		});
 		const response = await fetch(`${this.urlValue}?${params.toString()}`);
 		this.resultTarget.innerHTML = await response.text();
+	}
+	select(event) {
+		// console.log(event.currentTarget.dataset.objetId);
+		const selectedId = event.currentTarget.dataset.objetId;
+		this.objetTarget.innerHTML = selectedId;
+
+		const cell = event.currentTarget.parentElement;
+		const row = cell.parentElement;
+		this.selectedTargets.map((targ) => {
+			const cell = targ.parentElement;
+			const row = cell.parentElement;
+			row.classList.remove("text-success");
+		});
+
+		row.classList.add("text-success");
 	}
 }
