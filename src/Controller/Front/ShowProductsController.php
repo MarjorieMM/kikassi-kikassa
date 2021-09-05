70236efb0ae123f9e8ddae57011e553f2a8e9b77
Marjorie Meziere <mmwebforce20@gmail.com>
2021-09-06 00:15:41 +0200

    htaccess added

--- src/Controller/Front/ShowProductsController.php
+++ src/Controller/Front/ShowProductsController.php
@@ -9,17 +9,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ShowProductsController extends AbstractController
 {
 
-    // #[Route('/{reactRouting}', name: 'index', priority: '-1', requirements: ['reactRouting' => '^(?!admin)(?!api).+'], defaults: ['reactRouting' => null])]
+    #[Route('/{reactRouting}', name: 'index', priority: '-1', requirements: ['reactRouting' => '^(?!admin)(?!api).+'], defaults: ['reactRouting' => null])]
 
     /**
      * @Route("/{reactRouting}", name="index", priority="-1", requirements={"reactRouting"="^(?!admin)(?!api).+"}, defaults={"reactRouting": null})
      */
 
-    // public function index(): Response
-    // {
+    public function index(): Response
+    {
 
-    //     return $this->render('front/show_products/index.html.twig', [
-    //         'controller_name' => 'ShowProductsController',
-    //     ]);
-    // }
+        return $this->render('front/show_products/index.html.twig', [
+            'controller_name' => 'ShowProductsController',
+        ]);
+    }
 }
